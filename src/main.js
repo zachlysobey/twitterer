44769b10c8618016a31ea28ef2444eb63c3038ff
Zach Lysobey <zlysobey@gmail.com>
2019-10-19 23:15:39 -0400

    👋 Hello dotenv!

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
+require('dotenv').config()
 const Koa = require('koa')
 const logger = require('koa-logger')
 
 const { router } = require('./router')
+
+const { TEST_ENV_VARIABLE } = process.env
+
+if (TEST_ENV_VARIABLE) {
+    console.log(TEST_ENV_VARIABLE)
+} else {
+    console.warn(
+        'missing TEST_ENV_VARIABLE env variable.',
+        'did you remember to "cp .env.sample .env"?',
+    )
+}
 
 const app = new Koa()
 
